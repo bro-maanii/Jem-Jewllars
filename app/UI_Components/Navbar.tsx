e6c407374575e535313485d59ec8bceca37f1ebf
bro-maanii <emanaliabbasi31@gmail.com>
2024-08-21 19:11:51 +0500

    refactor: Optimize Navbar component SVG icons

--- app/UI_Components/Navbar.tsx
+++ app/UI_Components/Navbar.tsx
@@ -35,104 +35,106 @@ const Navbar = () => {
       />
       {/* cart */}
       <div className="hidden md:flex justify-end w-full">
-  <svg
-    width="40px"
-    height="40px"
-    viewBox="-2.4 -2.4 28.80 28.80"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
-  >
-    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-    <g
-      id="SVGRepo_tracerCarrier"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      stroke="#ffffff"
-      strokeWidth="0.048"
-    />
-    <g id="SVGRepo_iconCarrier">
-      {"{"}" "{"}"}
-      <path
-        d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
-        stroke="#ffffff"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      {"{"}" "{"}"}
-    </g>
-  </svg>
-</div>
+        <svg
+          width="40px"
+          height="40px"
+          viewBox="-2.4 -2.4 28.80 28.80"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            stroke="#ffffff"
+            strokeWidth="0.048"
+          />
+          <g id="SVGRepo_iconCarrier">
+            &quot;{"{"}&quot;{"}"}
+            <path
+              d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+              stroke="#ffffff"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            &quot;{"{"}&quot;{"}"}
+          </g>
+        </svg>
+      </div>
 
       {/* Menu */}
       <div onClick={handleNav} className="md:hidden">
-            <svg
-    width="50px"
-    height="50px"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    stroke="#ffffff"
-  >
-    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-    <g
-      id="SVGRepo_tracerCarrier"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <g id="SVGRepo_iconCarrier">
-      {"{"}" "{"}"}
-      <path
-        d="M4 6H20M4 12H20M4 18H20"
-        stroke="#ffffff"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      {"{"}" "{"}"}
-    </g>
-  </svg>
-</div>
-
+        <svg
+          width="50px"
+          height="50px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#ffffff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <g id="SVGRepo_iconCarrier">
+            &quot;{"{"}&quot;{"}"}
+            <path
+              d="M4 6H20M4 12H20M4 18H20"
+              stroke="#ffffff"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            &quot;{"{"}&quot;{"}"}
+          </g>
+        </svg>
+      </div>
 
       {menu && (
         <>
           <div className="absolute top-0 left-0 w-full h-screen bg-black flex flex-col items-center justify-center">
-            <div className="absolute top-3 right-6 text-3xl " onClick={handleNav}>
+            <div
+              className="absolute top-3 right-6 text-3xl "
+              onClick={handleNav}
+            >
               X
             </div>
             {/* Cart */}
             <div className="absolute top-3 left-6 text-xl">
-  <svg
-    width="40px"
-    height="40px"
-    viewBox="-2.4 -2.4 28.80 28.80"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
-  >
-    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-    <g
-      id="SVGRepo_tracerCarrier"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      stroke="#ffffff"
-      strokeWidth="0.048"
-    />
-    <g id="SVGRepo_iconCarrier">
-      {"{"}" "{"}"}
-      <path
-        d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
-        stroke="#ffffff"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      {"{"}" "{"}"}
-    </g>
-  </svg>
-</div>
+              <svg
+                width="40px"
+                height="40px"
+                viewBox="-2.4 -2.4 28.80 28.80"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  stroke="#ffffff"
+                  strokeWidth="0.048"
+                />
+                <g id="SVGRepo_iconCarrier">
+                  &quot;{"{"}&quot;{"}"}
+                  <path
+                    d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+                    stroke="#ffffff"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  &quot;{"{"}&quot;{"}"}
+                </g>
+              </svg>
+            </div>
 
             <ul className="flex flex-col gap-5">
               <li className="text-2xl hover:delay-100 hover:scale-110 hover:font-bold ">
